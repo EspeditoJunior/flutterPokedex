d0a7c75496dfba98b95a816d94aed5cd94609a01
Espedito Junior <EspeditoJunior@Cignifis-MacBook-Pro.local>
2020-01-19 10:08:07 -0300

    Aula 1

---
+++ lib/consts/consts_app.dart
@@ -0,0 +1,8 @@
+class ConstsApp {
+
+  static const String whitePokeball = 'assets/images/pokeball.png';
+  static const String blackPokeball = 'assets/images/pokeball_dark.png';
+  
+
+
+}
---
+++ lib/pages/home_page/widgets/app_bar_home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class AppBarHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 20, right: 5),
+                  child: IconButton(
+                    icon: Icon(Icons.menu),
+                    onPressed: () {},
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Text('Pokedex',
+                    style: TextStyle(
+                        fontFamily: 'Google',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 28)),
+              ),
+            ],
+          )
+        ],
+      ),
+      color: Colors.blue,
+      height: 120
+    );
+  }
+}
